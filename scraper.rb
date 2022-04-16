fede9afa5585aea52b9b13e34bbb0c9092d48a16
Ytta <ytt0a44@gmail.com>
2022-04-16 22:50:23 +0300

    Added comments and command line arguments

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,16 @@
+# Script was developed and worked on Windows.
+# OpenURI module was used instead Curb.
 require 'byebug'
 require 'csv'
 require 'nokogiri'
 require 'open-uri'
 
-cat_url = "https://www.petsonic.com/dermatitis-y-problemas-piel-para-perros/"
-file_name = "products.csv"
+cat_url = ARGV[0] # https://www.petsonic.com/dermatitis-y-problemas-piel-para-perros/
+file_name = ARGV[1] # products.csv
 
 unparsed_page = URI.open(cat_url)
 parsed_page = Nokogiri::HTML(unparsed_page)
-all_product_link = []
+all_product_link = [] # Array for keeping links to products
 
 parsed_page.xpath(".//*[@id='product_list']/li/div/div[2]/div[2]/a/@href").each do |product_link|
   all_product_link << product_link.text
@@ -24,8 +26,10 @@ CSV.open(file_name, "wb") do |csv_line|
     product_img = product_page.xpath('.//img[@id="bigpic"]/@src').text
 
     product_page.xpath(".//div[@class='attribute_list']//ul/li").each do |variation|
+      # Object is created for each variation of product
+
       product = {
-        title: "#{product_title} - #{variation.xpath('.//span[@class="radio_label"]').text}",
+        title: "#{product_title} - #{variation.xpath('.//span[@class="radio_label"]').text}", # following the example
         price: variation.xpath('.//span[@class="price_comb"]').text,
         img_path: product_img
       }
